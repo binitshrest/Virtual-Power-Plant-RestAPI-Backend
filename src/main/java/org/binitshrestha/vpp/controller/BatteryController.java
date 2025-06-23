d1a88f969ed075f4e1ce13ff91472af043403259
Binit Shrestha <53391715+binitshrest@users.noreply.github.com>
2025-06-23 17:49:18 +0545

    write integration test using testcontainers

--- src/main/java/org/binitshrestha/vpp/controller/BatteryController.java
+++ src/main/java/org/binitshrestha/vpp/controller/BatteryController.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.binitshrestha.vpp.model.dto.*;
 import org.binitshrestha.vpp.model.entity.Battery;
 import org.binitshrestha.vpp.service.BatteryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
--- src/test/java/org/binitshrestha/vpp/controller/BatteryControllerIT.java
+++ src/test/java/org/binitshrestha/vpp/controller/BatteryControllerIT.java
@@ -1,31 +1,98 @@
 package org.binitshrestha.vpp.controller;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.binitshrestha.vpp.model.dto.BatteryCreateRequest;
+import org.binitshrestha.vpp.model.dto.BatteryCreateRequestBuilder;
+import org.binitshrestha.vpp.model.dto.BatteryResponse;
+import org.binitshrestha.vpp.model.dto.BatteryResponseBuilder;
+import org.binitshrestha.vpp.repository.BatteryRepository;
+import org.binitshrestha.vpp.service.BatteryService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @DataJpaTest
-@Import(BatteryController.class)
+@Import({BatteryController.class, BatteryService.class})
+@AutoConfigureMockMvc
 @Testcontainers
 class BatteryControllerIT {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private BatteryService batteryService;
+
+    @Autowired
+    private BatteryRepository batteryRepository;
+
+    private BatteryCreateRequest request;
 
     @Container
     static PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"));
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"))
+                    .withUsername("postgres")
+                    .withPassword("password")
+                    .withExposedPorts(5432);
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url",postgres::getJdbcUrl);
+        registry.add("spring.datasource.username",postgres::getUsername);
+        registry.add("spring.datasource.password",postgres::getPassword);
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll(){
+        postgres.stop();
+    }
 
     @BeforeEach
     void setUp() {
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(new BatteryController(batteryService))
+                .build();
+        request = new BatteryCreateRequestBuilder().build();
 
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown(){
+        batteryRepository.deleteAll();
+    }
+
+    @Test
+    void testRegisterBattery() throws Exception {
+        // Act & Assert
+        mockMvc.perform(post("/api/v1/batteries")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("Duracell"))
+                .andExpect(jsonPath("$.postcode").value("44600"))
+                .andExpect(jsonPath("$.wattCapacity").value(150.0));
 
     }
 }
