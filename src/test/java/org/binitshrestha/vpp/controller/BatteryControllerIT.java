ee053f159fbe788958698c5cd8a6bfd14ef464bc
Binit Shrestha <53391715+binitshrest@users.noreply.github.com>
2025-06-24 21:59:21 +0545

    test: completed integration testing for vpp application

--- src/test/java/org/binitshrestha/vpp/controller/BatteryControllerIT.java
+++ src/test/java/org/binitshrestha/vpp/controller/BatteryControllerIT.java
@@ -1,6 +1,7 @@
 package org.binitshrestha.vpp.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.binitshrestha.vpp.exception.BatteryNotFoundException;
 import org.binitshrestha.vpp.model.dto.BatteryBatchCreateRequest;
 import org.binitshrestha.vpp.model.dto.BatteryBatchCreateRequestBuilder;
 import org.binitshrestha.vpp.model.dto.BatteryCreateRequest;
@@ -22,8 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -103,5 +103,120 @@ class BatteryControllerIT extends BaseIntegrationTest{
                 .andExpect(jsonPath("$[0].postcode").value("44600"))
                 .andExpect(jsonPath("$[1].name").value("Battery B"))
                 .andExpect(jsonPath("$[1].postcode").value("44600"));
+    }
+
+    @Test
+    void testGetBatteryById() throws Exception {
+        Long batteryId = batteryRepository.findByName("Battery A").map(Battery::getId).orElseThrow(() -> new BatteryNotFoundException("Battery Not Found"));
+        // Act & Assert
+        mockMvc.perform(get("/api/v1/batteries/{id}", batteryId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Battery A"))
+                .andExpect(jsonPath("$.postcode").value("44600"));
+    }
+
+    @Test
+    void testGetBatteriesInPostcodeRange() throws Exception {
+        // Arrange
+        String startPostcode = "40000";
+        String endPostcode = "50000";
+        Double minWattCapacity = 100.0;
+        Double maxWattCapacity = 500.0;
+        // Act & Assert
+        mockMvc.perform(get("/api/v1/batteries/postcode-range")
+                        .param("startPostcode", startPostcode)
+                        .param("endPostcode", endPostcode)
+                        .param("minWattCapacity", String.valueOf(minWattCapacity))
+                        .param("maxWattCapacity", String.valueOf(maxWattCapacity))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testUpdateBatteryStatus() throws Exception {
+        // Arrange
+        Long batteryId = batteryRepository.findByName("Battery A").map(Battery::getId).orElseThrow(() -> new BatteryNotFoundException("Battery Not Found"));
+        Battery.BatteryStatus newStatus = Battery.BatteryStatus.ACTIVE;
+
+        // Act & Assert
+        mockMvc.perform(put("/api/v1/batteries/{id}/status", batteryId)
+                        .param("status", newStatus.name())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Battery A"))
+                .andExpect(jsonPath("$.postcode").value("44600"));
+    }
+
+    @Test
+    void testGetHighCapacityBatteries() throws Exception {
+        // Arrange
+        Double minCapacity = 100.0;
+        // Act & Assert
+        mockMvc.perform(get("/api/v1/batteries/high-capacity")
+                        .param("minCapacity", String.valueOf(minCapacity))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Battery A"))
+                .andExpect(jsonPath("$[0].postcode").value("44600"))
+                .andExpect(jsonPath("$[1].name").value("Battery B"))
+                .andExpect(jsonPath("$[1].postcode").value("44600"));
+    }
+    @Test
+    void testGetStatisticsByPostcode() throws Exception {
+        // Act & Assert
+        mockMvc.perform(get("/api/v1/batteries/statistics/by-postcode")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].postcode").value("44600"))
+                .andExpect(jsonPath("$[0].batteryCount").value(2))
+                .andExpect(jsonPath("$[0].totalCapacity").value(300.0));
+
+    }
+
+    @Test
+    void testGetTotalBatteryCount() throws Exception {
+        // Act & Assert
+        mockMvc.perform(get("/api/v1/batteries/count")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.count").value(saveBatteries.size()));
+    }
+
+    @Test
+    void testSearchBatteriesByName() throws Exception {
+        // Arrange
+        String namePattern = "Battery";
+        // Act & Assert
+        mockMvc.perform(get("/api/v1/batteries/search")
+                        .param("namePattern", namePattern)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Battery A"))
+                .andExpect(jsonPath("$[0].postcode").value("44600"))
+                .andExpect(jsonPath("$[1].name").value("Battery B"))
+                .andExpect(jsonPath("$[1].postcode").value("44600"));
+    }
+
+    @Test
+    void testGetUniquePostcodes() throws Exception {
+        // Act & Assert
+        mockMvc.perform(get("/api/v1/batteries/postcodes")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").value("44600"));
+    }
+
+    @Test
+    void testHealthCheck() throws Exception {
+        // Arrange
+        int totalBatteries = 2; // Example value;
+        // Act & Assert
+        mockMvc.perform(get("/api/v1/batteries/health")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"))
+                .andExpect(jsonPath("$.message").value("Battery service is operational"))
+                .andExpect(jsonPath("$.totalBatteries").value(totalBatteries));
     }
 }
